1df30ad5462dffe7535494c8710a1fb8f8db2e65
Aneesh <75196425+Aneesh1303@users.noreply.github.com>
2023-01-28 12:24:57 +0530

    Added solution - LeetHub

--- K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
@@ -8,33 +8,29 @@ class Solution{
     public:
     int kthElement(int arr1[], int arr2[], int n, int m, int k)
     {
-        int i = 0;
-        int j = 0;
-        int arr[n+m];
-        int l = 0;
+        if(n > m) return kthElement(arr2, arr1, m, n, k);
         
-        while(i<n && j<m) {
-            if(arr1[i] <= arr2[j]) {
-                arr[l++] = arr1[i++];
+        int low = max(0,k-m);
+        int high = min(k,n);
+        
+        while(low <= high) {
+            int cut1 = (low+high) >> 1;
+            int cut2 = k - cut1;
+            
+            int l1 = cut1 == 0 ? INT_MIN : arr1[cut1-1];
+            int l2 = cut2 == 0 ? INT_MIN : arr2[cut2-1];
+            int r1 = cut1 == n ? INT_MAX : arr1[cut1];
+            int r2 = cut2 == m ? INT_MAX : arr2[cut2];
+            
+            if(l1 <= r2 && l2 <= r1) {
+                return max(l1, l2);
             }
-            else {
-                arr[l++] = arr2[j++];
+            else if(l1 > r2) {
+                high = cut1-1;
             }
+            else low = cut1+1;
         }
-        
-        if(i<n) {
-            while(i<n) {
-                arr[l++] = arr1[i++];
-            }
-        }
-        
-        if(j<m) {
-            while(j<m) {
-                arr[l++] = arr2[j++];
-            }
-        }
-        
-        return arr[k-1];
+        return 1;
     }
 };
 
